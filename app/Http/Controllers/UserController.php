e6f0d57df1283167a578d4a6ae78e9b3e3bb987c
unknown <ashikbur007@gmail.com>
2023-07-27 19:54:32 +0600

    vv

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Throwable;
 
 class UserController extends Controller
 {
     //
     function userRegistration(Request $request){
+
+        try {
+            User::create([
+                'firstName' =>$request->input('firstName'),
+                'lastName' =>$request->input('lastName'),
+                'email' =>$request->input('email'),
+                'mobile' =>$request->input('mobile'),
+                'password' =>$request->input('password')
+
+        ]);
+
+        return response()->json([
+            'status'=>'success',
+            'message'=>'Registration successful'
+        ]);
+        } catch (Throwable $th) {
+            return response()->json([
+                'status'=>'failure',
+                'message'=> $th->getMessage()
+
+
+            ]);
+        }
+
 
     }
 }
